045de3e530abf249a643b66cad198266d269e8f6
rumcajsek <97509945+rumcajsek@users.noreply.github.com>
2024-03-03 19:37:49 +0100

    added some nav styles

---
+++ src/main/resources/static/scripts/mainScript.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+function greet() {
+    alert("Hello, World!");
+}
