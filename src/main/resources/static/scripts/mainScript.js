e452bae41dc191f3c1fc44c81dcb6d9bf7d07be4
rumcajsek <97509945+rumcajsek@users.noreply.github.com>
2024-03-25 14:20:15 +0100

    Scripts added

--- src/main/resources/static/scripts/mainScript.js
+++ src/main/resources/static/scripts/mainScript.js
@@ -10,6 +10,16 @@ $("#navbarToggler").on("click", function () {
     $nav.toggleClass("shadowed");
 });
 
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
+
 function greet() {
     alert("Hello, World!");
 }
